cc3400cbe5b42520218f15145720e63d979d2515
Rinery <91065334+Rinery@users.noreply.github.com>
2024-11-12 13:32:29 +0300

    Добавлен модуль и библиотека material

--- app/src/main/java/com/example/speakfreely/app/MainActivity.kt
+++ app/src/main/java/com/example/speakfreely/app/MainActivity.kt
@@ -6,42 +6,43 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.speakfreely.app.ui.theme.SpeakFreelyTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        enableEdgeToEdge() // экран растягивается от края до края
         setContent {
             SpeakFreelyTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+
                 }
             }
         }
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+    @Composable
+    fun BottomNavigationBar () {
+        BottomNavigation(
+            contentColor = MaterialTheme.colors.primary
+        ) {
+            BottomNavigationItem (
+                icon = {
+                    ImageVector.vectorResource(id = R.drawable.)
+                },
+                selected = false,
+                onClick = {
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    SpeakFreelyTheme {
-        Greeting("Android")
+                }
+            )
+        }
     }
 }
