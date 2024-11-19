45aa64c2a6cfabd6341948a6065418101dc8867f
Rinery <91065334+Rinery@users.noreply.github.com>
2024-11-19 11:27:02 +0300

    Интегрирован API перевода (https://ftapi.pythonanywhere.com)

--- app/src/main/java/com/example/speakfreely/app/core/domain/TranslationUseCase.kt
+++ app/src/main/java/com/example/speakfreely/app/core/domain/TranslationUseCase.kt
@@ -2,11 +2,13 @@ package com.example.speakfreely.app.core.domain
 
 import jakarta.inject.Inject
 import com.example.speakfreely.app.core.TranslationApi
+import com.example.speakfreely.app.core.TranslationResponse
 
 class TranslationUseCase @Inject constructor(
     private val translationApi: TranslationApi
 ) {
-    suspend fun translate() {
-
+    // sl - source lang, dl - destination lang
+    suspend fun translate(sl: String, dl: String, text: String): TranslationResponse {
+        return translationApi.translate(sl, dl, text)
     }
 }
