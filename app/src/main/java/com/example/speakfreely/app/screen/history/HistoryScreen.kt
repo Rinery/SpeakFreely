0328ff1e5f346f4a12048540ec69190f0cbfb5ea
Rinery <91065334+Rinery@users.noreply.github.com>
2024-11-20 08:35:30 +0300

    Добавлена экспериментальная функция @OptIn(ExperimentalMaterial3Api::class) в HistoryScreen.kt для поддержки элемента TopAppBar

--- app/src/main/java/com/example/speakfreely/app/screen/history/HistoryScreen.kt
+++ app/src/main/java/com/example/speakfreely/app/screen/history/HistoryScreen.kt
@@ -2,12 +2,14 @@ package com.example.speakfreely.app.screen.history
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HistoryScreen(
     viewModel: HistoryViewModel = hiltViewModel()
